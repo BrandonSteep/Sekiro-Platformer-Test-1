40bb374cba2d5bdf08712f0c3c1b4cd7529c3612
Brandon Steep <jacobwetherall@hotmail.co.uk>
2024-11-23 16:23:03 +0000

    Add Player Sprites

--- Assets/Scripts/Player/Mele.cs
+++ Assets/Scripts/Player/Mele.cs
@@ -6,11 +6,18 @@ public class Mele : MonoBehaviour
 {
     [SerializeField]
     int damage = 10;
-    private void FixedUpdate()
-    {
+
+    private void Update(){
         if(Input.GetMouseButtonDown(0)) 
         {
             Debug.Log("left mouse pressed");
         }
     }
+
+    private void FixedUpdate()
+    {
+
+    }
+
+    
 }
