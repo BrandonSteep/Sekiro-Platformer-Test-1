db92c8843dc4fcb33720611bdba58c95f0c188dc
Brandon Steep <jacobwetherall@hotmail.co.uk>
2024-11-25 13:36:26 +0000

    Add Variables to EnemyStats, Add menuName to Player Stats Script

--- Assets/Scripts/Enemy/EnemyStats.cs
+++ Assets/Scripts/Enemy/EnemyStats.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyStats : MonoBehaviour
+[CreateAssetMenu (menuName = "Stats/Enemy Stats")]
+public class EnemyStats : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] private int health = 3;
+    [SerializeField] private int damage = 1;
+    [SerializeField] private float speed = 3f;
+    [SerializeField] private float attackRate = 2f;
+
+    public int GetHealth(){
+        return health;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public int GetDamage(){
+        return damage;
+    }
+
+    public float GetSpeed(){
+        return speed;
+    }
+
+    public float GetAttackRate(){
+        return attackRate;
     }
 }
