4a77ae4f1dd91002dda08a8d0461d655e642d635
Brandon Steep <jacobwetherall@hotmail.co.uk>
2024-11-25 18:00:51 +0000

    Add Pushback in Enemy's TakeDamage method

--- Assets/Scripts/Enemy/EnemyStatus.cs
+++ Assets/Scripts/Enemy/EnemyStatus.cs
@@ -7,13 +7,17 @@ public class EnemyStatus : MonoBehaviour, IStatus
     [SerializeField] private EnemyStats stats;
     [SerializeField] private int currentHealth;
 
+    [SerializeField] private Rigidbody2D rb;
+
     void OnEnable(){
+        rb = GetComponent<Rigidbody2D>();
         currentHealth = stats.GetHealth();
     }
 
     public void TakeDamage(int damageAmount, Vector2 hitPosition){
         Debug.Log($"Taken {damageAmount} points of damage");
         currentHealth -= damageAmount;
+        rb.AddForce(new Vector2(this.transform.position.x, this.transform.position.y) - hitPosition * damageAmount, ForceMode2D.Impulse);
         if(currentHealth <= 0){
             Die();
         }
