bfc3fcc730e7f2b5ed6faf12059b1354deb8dd52
Brandon Steep <jacobwetherall@hotmail.co.uk>
2024-11-25 19:12:13 +0000

    Fix Player Death, Add Component Deletion to Player Death to avoid errors

--- Assets/Scripts/Player/PlayerStatus.cs
+++ Assets/Scripts/Player/PlayerStatus.cs
@@ -33,6 +33,7 @@ public class PlayerStatus : MonoBehaviour, IStatus
     }
 
     public void Die(){
+        DestroyComponents();
         Destroy(this.gameObject);
     }
 
@@ -44,4 +45,24 @@ public class PlayerStatus : MonoBehaviour, IStatus
     private void RemoveIFrames(){
         canTakeDamage = true;
     }
+
+
+    #region Component Destruction
+    public Component[] componentsToDestroyOnDeath;
+
+    void DestroyComponents()
+    {
+        // Ensure there are components to destroy
+        if (componentsToDestroyOnDeath != null)
+        {
+            for (int i = 0; i < componentsToDestroyOnDeath.Length; i++)
+            {
+                if (componentsToDestroyOnDeath[i] != null)
+                {
+                    Destroy(componentsToDestroyOnDeath[i]);
+                }
+            }
+        }
+    }
+    #endregion
 }
