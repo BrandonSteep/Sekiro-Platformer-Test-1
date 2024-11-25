5c18d1ad27e71b471c435ac8e7295337fce48b0e
Brandon Steep <jacobwetherall@hotmail.co.uk>
2024-11-25 18:29:05 +0000

    Add TakeDamage and IFrames functionality to PlayerStatus

--- Assets/Scripts/Player/PlayerStatus.cs
+++ Assets/Scripts/Player/PlayerStatus.cs
@@ -7,12 +7,25 @@ public class PlayerStatus : MonoBehaviour, IStatus
     [SerializeField] private ScriptableVariable_int maxHealth;
     [SerializeField] private ScriptableVariable_int currentHealth;
 
+    [SerializeField] private bool canTakeDamage = true;
+    [SerializeField] private float iFramesInSeconds;
+
     void OnEnable(){
         currentHealth.SetValue(maxHealth.GetValue());
     }
 
     public void TakeDamage(int damageAmount, Vector2 hitPosition){
+        if(canTakeDamage){
+            Debug.Log($"Player Takes {damageAmount} points of Damage");
+            currentHealth.AddToValue(-damageAmount);
 
+            if(currentHealth.GetValue() <= 0){
+                Die();
+            }
+            else{
+                AddIFrames();
+            }
+        }
     }
 
     public void Heal(int healAmount){
@@ -21,5 +34,14 @@ public class PlayerStatus : MonoBehaviour, IStatus
 
     public void Die(){
         Destroy(this.gameObject);
+    }
+
+    private void AddIFrames(){
+        canTakeDamage = false;
+        Invoke("RemoveIFrames", iFramesInSeconds);
+    }
+
+    private void RemoveIFrames(){
+        canTakeDamage = true;
     }
 }
